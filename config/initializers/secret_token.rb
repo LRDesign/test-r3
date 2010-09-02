efd117ff7771303e74b706ac6662c0e48c5ad740
Evan Dorn <evan@lrdesign.com>
2010-09-02 15:22:01 -0700

    initial version of blank app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestR3::Application.config.secret_token = 'fb85ef2ee37f16265a3c11eecf95af71197e776625ba6273a054b009de8fa36032d72cc89cb0aeafb1e9fa9297915b59c2f781f5166c9d1ebb30fc0d096185a1'
